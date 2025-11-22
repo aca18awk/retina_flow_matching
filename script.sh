ed43ee2f15eb98cc5c5641c8be0068836d42172e
Aleksandra Kulbaka <awkulbaka1@gmail.com>
2025-11-22 11:57:35 +0000

    script

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -p gpus48
+#SBATCH --gres gpu:1
+#SBATCH --nodelist luna
+#SBATCH --output=run_logs/train_slurm.%N.%j.log
+
+source /vol/biomedic3/awk24/miniconda3/bin/activate
+conda activate diffusion
+
+export RDMAV_FORK_SAFE=1
+export OPENAI_LOG_FORMAT="stdout,log,csv,tensorboard"
+export OPENAI_LOGDIR="/vol/biomedic3/awk24/code/conditional-flow-matching/outputs"
+
+# inception_v3.tv_in1k
+# efficientnet_b0
+python conditional_retina.py
+# python conditional_mnist.py
