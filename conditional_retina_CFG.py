581685e2921696ddb6f98f486c9ebf75d6ba8a3f
Aleksandra Kulbaka <awkulbaka1@gmail.com>
2026-02-03 16:59:41 +0000

    refactor: move image generating functions outside the training file

--- conditional_retina_CFG.py
+++ conditional_retina_CFG.py
@@ -2,36 +2,42 @@ import os
 import torch
 import copy
 from torchvision import transforms
-from torchvision.transforms import ToPILImage
-from torchvision.utils import make_grid
 from torchcfm.conditional_flow_matching import *
 from torchcfm.conditional_flow_matching import ExactOptimalTransportConditionalFlowMatcher
 from torchcfm.models.unet import UNetModel
-import torchdiffeq
 from torchdyn.core import NeuralODE
 from retina_dataset import GlaucomaHarvardDataset
 from torch.utils.data import DataLoader
 import wandb
 import numpy as np
+from generate_img import generate_images
 
 # --- Configuration ---
-savedir = "models/cond_retina" 
+savedir = "models/2_Feb" 
 os.makedirs(savedir, exist_ok=True)
+
+figs_dir = os.path.join(savedir, "figs")
+os.makedirs(figs_dir, exist_ok=True)
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 batch_size = 32
-n_epochs = 200
+n_epochs = 5
 IMG_SIZE = 128
 NUM_CLASSES = 4
 NO_OF_CHANNELS_IMG = 3
 VALIDATION_SEED = 42 
-MAX_PLATEAU = 200
+MAX_PLATEAU = 1000
 NUM_CHANNELS_U_NET =128
 NUM_RES_BLOCKS_U_NET=2
 CHANNEL_MULT=(1, 2, 4, 8) # Deep semantics: 128->256->512->1024
 ATTENTION_RESOLUTIONS="32, 16, 8"
 TRAIN_AUGMENTATION = True
+GUIDANCE_SCALE = 3
+
+
+def save_model(model, save_path):
+    torch.save(model.state_dict(), save_path)
 
 # --- WandB Init ---
 logger = wandb.init(
@@ -46,7 +52,7 @@ logger = wandb.init(
         "lr": 1e-4,
         "channel_mult":CHANNEL_MULT, 
         "attention_resolutions":ATTENTION_RESOLUTIONS,
-        "data augmentation": "geometric only",
+        "data augmentation": "all",
         "classifier free guidance": True
     },
     resume="allow",
@@ -67,10 +73,10 @@ transform1 = transforms.Compose([
 
 # --- Datasets ---
 transform_train = transform1 if TRAIN_AUGMENTATION else transform
-train_dataset = GlaucomaHarvardDataset("train", transform_train, augmentation=TRAIN_AUGMENTATION)
+train_dataset = GlaucomaHarvardDataset("train", augmentation=TRAIN_AUGMENTATION)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-val_dataset = GlaucomaHarvardDataset("validation", transform, augmentation=False)
+val_dataset = GlaucomaHarvardDataset("validation", augmentation=False)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
 
@@ -88,6 +94,7 @@ model = UNetModel(
     channel_mult=CHANNEL_MULT, # Deep semantics: 128->256->512->1024
     attention_resolutions=ATTENTION_RESOLUTIONS, # Essential for global structure
 ).to(device)
+
 
 
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
@@ -113,7 +120,7 @@ for epoch in range(n_epochs):
         optimizer.zero_grad()
         x1 = data[0].to(device)
         real_label = data[1].to(device)
-        label = real_label if np.random.random() > 0.1 else torch.full_like(real_label, fill_value=3)
+        label = real_label if np.random.random() > 0.2 else torch.full_like(real_label, fill_value=3)
         x0 = torch.randn_like(x1)
 
         t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
@@ -152,6 +159,16 @@ for epoch in range(n_epochs):
         "train_loss": epoch_loss
     })
 
+    # --- Visualization & Local Saving (Every 100 Epochs) ---
+    if epoch % 100 == 0:
+        print(f"Generating visualization for epoch {epoch}...")
+                
+        generate_images(model, figs_dir, GUIDANCE_SCALE, epoch, run_name)
+
+        model_filename = f"model_{epoch}_{run_name}.pth"
+        save_path = os.path.join(savedir, model_filename)
+        save_model(model, save_path)
+
     # --- Early Stopping ---
     if val_epoch_loss <= best_loss:
         plateau_count = 0
@@ -168,59 +185,23 @@ for epoch in range(n_epochs):
 
 print("\nTraining complete. Loading best weights and generating images...")
 
+generate_images(model, figs_dir, GUIDANCE_SCALE, "last", run_name)
+
 model_filename = f"model_last_{run_name}.pth"
 save_path = os.path.join(savedir, model_filename)
-torch.save(model.state_dict(), save_path)
+save_model(model, save_path)
 if best_model is not None:
     model_filename = f"model_best_{run_name}.pth"
     save_path = os.path.join(savedir, model_filename)
     
     torch.save(best_model, save_path)
-    model.load_state_dict(best_model)
+
     print(f"Model saved to {save_path}")
 
-
-n_samples_per_class = 10
-total_samples = (NUM_CLASSES -1) * n_samples_per_class 
-generated_class_list = torch.arange((NUM_CLASSES -1), device=device).repeat_interleave(n_samples_per_class)
-no_label_list = torch.full_like(generated_class_list, fill_value=3)
-GUIDANCE_SCALE = 5
-
-def guided_vector_field(t, x):
-    t_vector = torch.full((x.shape[0],), fill_value=t, device=x.device)
-    v_cond = model.forward(t_vector, x, generated_class_list)
-    v_uncond = model.forward(t_vector, x, no_label_list)
-    v_final = v_uncond + GUIDANCE_SCALE * (v_cond - v_uncond)
-    return v_final
+    last_model = copy.deepcopy(model.state_dict())
+    model.load_state_dict(last_model)
+    generate_images(model, figs_dir, GUIDANCE_SCALE, "last_after_loading", run_name)
 
 
-model.eval()
-with torch.no_grad():
-    x0_noise = torch.randn(total_samples, NO_OF_CHANNELS_IMG, IMG_SIZE, IMG_SIZE, device=device)
-    traj = torchdiffeq.odeint(
-            lambda t, x: guided_vector_field(t, x),
-            x0_noise,
-            torch.linspace(0, 1, 2, device=device),
-            atol=1e-5, 
-            rtol=1e-5,
-            method="dopri5",
-        )
-
-final_images = traj[-1].view([-1, NO_OF_CHANNELS_IMG, IMG_SIZE, IMG_SIZE])
-
-grid = make_grid(
-    final_images.clip(-1, 1), 
-    value_range=(-1, 1),
-    normalize=True, 
-    padding=2, 
-    nrow=n_samples_per_class
-)
-
-img = ToPILImage()(grid)
-image_filename = f"retina_gen_{run_name}.png"
-output_image_path = os.path.join(savedir, image_filename)
-
-img.save(output_image_path)
-print(f"Saved generated image to {output_image_path}")
 
 wandb.finish()
